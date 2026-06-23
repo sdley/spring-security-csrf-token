fb995b404158b8ebbf4b5ef6a8086a17f5164dbc
Souleymane DIALLO <sdley2020@gmail.com>
2026-06-23 17:52:46 +0000

    add SecurityConfig class to configure Spring Security settings

---
+++ src/main/java/sn/sdley/my_spring_boot_app2_security/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package sn.sdley.my_spring_boot_app2_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(customizer -> customizer
+                        .requestMatchers("/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(customizer -> customizer
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout(customizer -> customizer
+                        .logoutRequestMatcher(PathPatternRequestMatcher.pathPattern(HttpMethod.GET, "/logout"))
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
