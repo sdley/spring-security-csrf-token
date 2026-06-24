0bb7fc0e8ff0e7bc95d3c4b6a20747767daa3884
Souleymane DIALLO <sdley2020@gmail.com>
2026-06-24 09:02:36 +0000

    refactor SecurityConfig to use method reference for CSRF disabling

--- src/main/java/sn/sdley/my_spring_boot_app2_security/config/SecurityConfig.java
+++ src/main/java/sn/sdley/my_spring_boot_app2_security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 
-        http.csrf(customizer -> customizer.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(customizer -> customizer
                         .requestMatchers("/login", "/error").permitAll()
                         .anyRequest().authenticated()
