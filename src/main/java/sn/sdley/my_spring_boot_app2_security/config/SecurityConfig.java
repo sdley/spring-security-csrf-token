187676f3a3158ea59954627201d8eefcac0ff754
Souleymane DIALLO <sdley2020@gmail.com>
2026-07-01 12:01:57 +0000

    add in-memory user details service with sample users for authentication

--- src/main/java/sn/sdley/my_spring_boot_app2_security/config/SecurityConfig.java
+++ src/main/java/sn/sdley/my_spring_boot_app2_security/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,5 +44,24 @@ public class SecurityConfig {
                         .permitAll()
                 );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("sdley")
+                .password("passer")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("user2")
+                .password("password2")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
