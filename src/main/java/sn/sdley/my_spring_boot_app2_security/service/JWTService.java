7cac594a8a35add7c37b2e9f93be7d4bd4e5c09e
Souleymane DIALLO <sdley2020@gmail.com>
2026-07-08 11:43:19 +0000

    remove unused UserService dependency from JWTService

--- src/main/java/sn/sdley/my_spring_boot_app2_security/service/JWTService.java
+++ src/main/java/sn/sdley/my_spring_boot_app2_security/service/JWTService.java
@@ -5,11 +5,7 @@ import sn.sdley.my_spring_boot_app2_security.model.User;
 
 @Service
 public class JWTService {
-    private final UserService userService;
-    
-    public JWTService(UserService userService) {
-        this.userService = userService;
-    }
+
 
     public String generateToken(User user) {
         // The JWT implementation will go here
